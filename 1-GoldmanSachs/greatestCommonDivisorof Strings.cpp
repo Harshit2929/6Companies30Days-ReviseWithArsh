316a56a58128938cca10691e3bbe8b3a35435419
Harshit <harshitchauhan12102001@gmail.com>
2022-01-06 19:30:24 +0530

    some solutions added

---
+++ 1-GoldmanSachs/greatestCommonDivisorof Strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+//        string temp1=str1;
+//        string temp2=str2;
+        
+        // string ans;
+        
+       if((str1+str2!=str2+str1)) {
+           return "";
+       }
+        else if (str1==str2){
+            return str1;             
+        }
+        
+        else if(str1.length()>str2.length()){
+            return gcdOfStrings(str1.substr(str2.length()),str2);            
+        }
+        
+        else{
+                return gcdOfStrings(str2.substr(str1.length()),str1);                                    
+        }
+        
+                
+      // return ans;
+        
+        
+    }
+};
